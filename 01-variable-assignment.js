41d2faf8a2910c9eeababc44afb9a9c9712885ff
Pranay bawane <pranaybawane1924@gmail.com>
2025-07-29 09:09:07 +0530

    Variable Assignment
    Variable Assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,19 @@
+
+var cityName = "Amravati";
+console.log("My city name is: "+ cityName);
+
+var bestFriend = "Thomas";
+console.log("My best friend name is: "+ bestFriend);
+
+var country="India";
+console.log("My country name is: "+ country);
+
+var schoolName="Golden Kids Highschool";
+console.log("My School Name is: "+ schoolName);
+
+var collegeName ="Govt. College of Engineering Abad";
+console.log("My College name is: "+collegeName);
+
+
+
+
---
+++ 01-variableAssign.js
@@ -0,0 +1,45 @@
+var myFullName ;
+
+console.log("My full name is: "+myFullName);
+
+var myFullName= "Pranay Bawane";
+console.log("My Full Name is: "+myFullName);
+
+myFullName ="Pranay Vinayakrao Bawane";
+console.log("My Full Name is: "+myFullName);
+
+console.log("==============================================================");
+
+var myLove ="I love only JS";
+console.log("My Love: "+myLove);
+
+var firstName="Pranay";
+console.log("First Name: "+firstName);
+
+var lastName = "Bawane";
+console.log("Last Name: "+lastName);
+
+var age="26";
+console.log("Age: "+age);
+
+var address="Gadge Nagar Amravati";
+console.log("Address: "+address);
+
+var pinCode="444603";
+console.log("PinCode: "+pinCode);
+
+var gender= "Male";
+console.log("Gender: "+gender);
+
+var isMarried="No";
+console.log("Is married: "+isMarried);
+
+
+
+
+
+
+
+
+
+
